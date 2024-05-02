31550e088ea380211cf774d2ff7aa6e81887cd43
Georgi 7DIGIT <georgi@7digit.io>
2024-05-02 23:52:52 +0300

    Fix: remove unnecessary code

--- src/app.tsx
+++ src/app.tsx
@@ -40,51 +40,13 @@ const Component = () => {
         <button
           className="button-33"
           onClick={() => {
+            ipcRenderer.send("disconnect-socket");
             ipcRenderer.send("reconnect-socket");
           }}
         >
-          Свържи четец
-        </button>
-        <button
-          className="button-33"
-          onClick={() => {
-            ipcRenderer.send("disconnect-socket");
-          }}
-        >
-          Прекрати връзката
+          Рестартирай връзката с четец
         </button>
       </div>
-
-      {placeData.displayData ? (
-        placeData.isConnected ? (
-          <h2>Обектът е свързан</h2>
-        ) : (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <h2>Обектът не е регистриран</h2>
-            <p>{placeData.macAddress}</p>
-          </div>
-        )
-      ) : null}
-
-      {!placeData.isConnected && (
-        <button
-          className="button-33"
-          onClick={() => {
-            ipcRenderer.send("connect-place");
-          }}
-        >
-          {placeData.hasTriedToConnect
-            ? "Опитай ново свързване"
-            : "Свържи обект"}
-        </button>
-      )}
     </div>
   );
 };
